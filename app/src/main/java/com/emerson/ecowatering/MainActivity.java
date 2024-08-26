2ab3b7400a71aebebeccb706f70528960743a596
DevCodeEmersonRomero <emerson113@gmail.com>
2024-08-26 18:06:47 -0500

    Proyecto "ecowatering", Reto diplomado UN.

--- app/src/main/java/com/emerson/ecowatering/MainActivity.java
+++ app/src/main/java/com/emerson/ecowatering/MainActivity.java
@@ -1,6 +1,9 @@
 package com.emerson.ecowatering;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button btnIngresar;
+    //Declaración de la variable indicando el tipo de control
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnIngresar = findViewById(R.id.btn_ingresar_m);
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistroActivity.class);
+                startActivity(intent);
+
+                //codigo para ir a otra actividad indicando cual actividad es a la que quiero ir
+
+            }
+        });
+
+
     }
 }
